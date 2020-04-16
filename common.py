9f849b02154f13c9059d999b5106f4af230765ad
markryanandrews <markryanandrews794@gmail.com>
2020-04-16 17:02:48 -0400

    added tsplot function

--- common.py
+++ common.py
@@ -1,5 +1,12 @@
 import datetime as dt
+import numpy as np
+import statsmodels.graphics.tsaplots as smt
 import pandas as pd
+import matplotlib.pyplot as plt
+import seaborn as sns
+
+
+### DATA CLEANING / MANIPULATING FUNCTIONS ###
 
 def log_frame(f):
     def wrapper(dataf, *args, **kwargs):
@@ -68,3 +75,37 @@ def item_sales(item_name, sales_df, calendar_df):
     return item_sale_df.iloc[:,:1]
     
 
+### TIME SERIES FUNCTIONS ### 
+def tsplot(y , title, lags = None, diff = 0, figsize = (16,8)):
+    '''
+    Examine patterns with
+    Time Series plot,
+    Histogram plot,
+    ACF and PACF plots
+    '''
+    
+    if diff > 5:
+        return "Error: diff too high, Please choose [0-5]"
+    if diff > 0:
+        y = y.diff(diff)[diff:]  
+    
+    fig, ((ts_ax, hist_ax), (acf_ax, pacf_ax)) = plt.subplots(2, 2, figsize=figsize)
+    #fig.suptitle('Time Series Analysis', fontsize =16)
+
+    y.plot(ax = ts_ax)
+    ts_ax.set_title(title, fontsize = 14, fontweight = 'bold')
+    y.plot(ax = hist_ax, kind = 'hist', bins = 25)
+    hist_ax.set_title(title, fontsize = 14, fontweight = 'bold')
+    smt.plot_acf(y, lags = lags, ax = acf_ax)
+    smt.plot_pacf(y, lags = lags, ax = pacf_ax)
+    [ax.set_xlim(-.5) for ax in [acf_ax, pacf_ax]]
+    sns.despine()
+    plt.tight_layout()
+    return ts_ax, hist_ax, acf_ax, pacf_ax
+    
+    
+    
+    
+    
+    
+    
